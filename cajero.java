f008b11290c8b2daaed2c98f18c3f7ba11f10ce0
Saida <saidarozo03@gmail.com>
2026-02-12 15:59:42 -0500

    Plantilla

---
+++ cajero.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CajeroAutomatico {
+
+    // Menu cajero
+
+
+
+    // Consultar saldo y Retirar dinero
+
+
+
+
+    // Depositar dinero
+    
+
+
+    // Validaciones
+}
