9457e9a64981218e80fa1f7842674510528df9d6
cristianbg2007 <cristianfelipebeltrangonzalez@gmail.com>
2026-02-12 16:58:20 -0500

    cambios de retiro de dinero con JOPtion

--- cajero.java
+++ cajero.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class CajeroAutomatico {
 
@@ -7,29 +8,29 @@ public class CajeroAutomatico {
 
 
     // Consultar saldo y Retirar dinero
+ case 2:
 
-
-
-
-    // Depositar dinero
-    case 2:
-        system.out.print(" Ingrese el monto de dinero que quiere retirar: ");
-
-        double retiro = sc.nextDouble();
+        double retiro = Double.parseDouble(
+            JOptionPane.showInputDialog" Ingrese el monto que quiera retirar:"
+        );
 
         if(retiro> 0 && retiro<= saldo){
             
             saldo-=retiro;
-            system.out.println(" su retiro de dinero fue exitoso.");
-            system.out.println(" su nuevo saldo es de: " + saldo);
+            JOptionPane.showMessageDialog(null, "Retiro exitoso.\n ingrese el nuevo saldo " + saldo);
+
         }else{
 
-            system.out.println("no tiene el monto suficiente ingresado o es incorrecto";)
+            JOptionPane.showMessageDialog(null,"no tiene el monto suficiente ingresado o es incorrecto";)
         }
         break;
 
-                default:
-                    System.out.println("Opcion invalida.");
+          
+
+
+
+    // Depositar dinero
+   
 
 
     
