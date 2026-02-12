8fafbe5106c85a626c916f98c876ecf1d93d8362
cristianbg2007 <cristianfelipebeltrangonzalez@gmail.com>
2026-02-12 16:34:32 -0500

    parte retiro

--- cajero.java
+++ cajero.java
@@ -12,6 +12,26 @@ public class CajeroAutomatico {
 
 
     // Depositar dinero
+    case 2:
+        system.out.print(" Ingrese el monto de dinero que quiere retirar: ");
+
+        double retiro = sc.nextDouble();
+
+        if(retiro> 0 && retiro<= saldo){
+            
+            saldo-=retiro;
+            system.out.println(" su retiro de dinero fue exitoso.");
+            system.out.println(" su nuevo saldo es de: " + saldo);
+        }else{
+
+            system.out.println("no tiene el monto suficiente ingresado o es incorrecto";)
+        }
+        break;
+
+                default:
+                    System.out.println("Opcion invalida.");
+
+
     
 
 
